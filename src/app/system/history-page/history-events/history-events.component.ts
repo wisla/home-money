52910cb4c83a5b77f27f3e9e46682a1ffc9fd361
andrzej.wislowski@combeenut.com <andrzej.wislowski@combeenut.com>
2017-12-18 20:45:05 +0100

    first commit

---
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wisla-history-events',
+  templateUrl: './history-events.component.html',
+  styleUrls: ['./history-events.component.scss']
+})
+export class HistoryEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
